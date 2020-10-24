80179dd6792d42ae5ff40e1a3fa98b778ecd4bef
Max <mskravec@buffalo.edu>
2020-10-24 13:06:10 -0400

    Added Matrix

---
+++ Linear/Matrix.java
@@ -0,0 +1,13 @@
+package Linear;
+import Linear.Vector;
+
+import java.util.ArrayList;
+
+public class Matrix {
+    public ArrayList<Vector> matrix;
+
+    public Matrix(ArrayList<Vector> input){
+        matrix = input;
+    }
+
+}
